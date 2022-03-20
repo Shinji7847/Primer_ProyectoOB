9b5ba9c6a58e0e2071a3bfc43a0dcca79831353c
fbeltrame <97996900+Shinji7847@users.noreply.github.com>
2022-03-20 19:23:26 -0300

    Add files via upload

---
+++ TiposDatosMain.java
@@ -0,0 +1,18 @@
+package proyecto1;
+
+public class TiposDatosMain {
+
+	public static void main(String[] args) {
+		
+		int numero1 = 10;
+		double numero2 = 15.5;
+		long numero3 = 3000;
+		boolean otoñoargentino = true;
+		String cadena = "¡Hola mundo!";
+		
+		System.out.println("La primer variable es " + numero1 + " , " + "la segunda " + numero2 + 
+				" , la tercera es " + numero3 + ". La variable booleana resulta " + otoñoargentino + 
+				" y la variable de tipo String es " + cadena);
+	}
+
+}
